846f9c3bae924c58d74606247bce50fc2adc2860
Arsh <arshaniket@gmail.com>
2024-04-19 10:23:29 +0530

    first commit

--- base/views.py
+++ base/views.py
@@ -16,7 +16,11 @@ from django.db import transaction
 
 from .models import Task
 from .forms import PositionForm
+from django.contrib.auth import logout
 
+def logout_user(request):
+    logout(request)
+    return redirect('login')
 
 class CustomLoginView(LoginView):
     template_name = 'base/login.html'
@@ -48,11 +52,15 @@ class RegisterPage(FormView):
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
+    def get_queryset(self):
+        return self.request.user.task_set.all()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['tasks'] = context['tasks'].filter(user=self.request.user)
-        context['count'] = context['tasks'].filter(complete=False).count()
+        
+        context['completed_tasks'] = self.request.user.task_set.filter(complete=True)
+        context['incomplete_tasks'] = self.request.user.task_set.filter(complete=False)
+        context['incomplete_tasks_count'] = context['incomplete_tasks'].count()
 
         search_input = self.request.GET.get('search-area') or ''
         if search_input:
